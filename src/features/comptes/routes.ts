e1ed92ad6e90ff5827196d20cf411e6c8378b379
rvuiv <raniabouhou76@gmail.com>
2026-06-16 15:06:38 +0200

    les routes pour le section Comptes

--- src/features/comptes/routes.ts
+++ src/features/comptes/routes.ts
@@ -10,4 +10,13 @@ const repository = new ComptesRepository(pool);
 const service = new ComptesService(repository);
 const controller = new ComptesController(service);
 
+router.get('/utilisateurs/:idUtilisateur', controller.getComptes);
+router.post('/utilisateurs/:idUtilisateur', controller.createCompte);
+
+router.get('/:idCompte', controller.getCompteById);
+router.put('/:idCompte', controller.updateCompte);
+router.delete('/:idCompte', controller.deleteCompte);
+
+router.get('/:idCompte/solde', controller.getSolde);
+
 export default router;
