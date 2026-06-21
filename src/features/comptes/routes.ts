638cdd76220b29405382a3cd13044e7f564bd6d4
rvuiv <raniabouhou76@gmail.com>
2026-06-21 18:40:28 +0200

    changement de l'appel des routes dans "app.ts" et "routes.ts"

--- src/features/comptes/routes.ts
+++ src/features/comptes/routes.ts
@@ -4,24 +4,26 @@ import { ComptesService } from './service';
 import { ComptesRepository } from './repository';
 import { pool } from '../../config/database';
 
-const router = Router();
-
 const repository = new ComptesRepository(pool);
 const service = new ComptesService(repository);
 const controller = new ComptesController(service);
 
-router.get('/:idUtilisateur/comptes', controller.getComptes);
-router.post('/:idUtilisateur/comptes', controller.createCompte);
+// Router 1 : à monter sur /api/v1/utilisateurs
+export const comptesUtilisateursRouter = Router();
+comptesUtilisateursRouter.get('/:idUtilisateur/comptes', controller.getComptes);
+comptesUtilisateursRouter.post('/:idUtilisateur/comptes', controller.createCompte);
 
-router.options('/:idCompte', (req: Request, res: Response) => {
+// Router 2 : à monter sur /api/v1/comptes
+export const comptesRouter = Router();
+
+comptesRouter.options('/:idCompte', (req: Request, res: Response) => {
     res.set('Allow', 'GET, PUT, DELETE, OPTIONS');
     res.status(204).send();
 });
 
-router.get('/:idCompte', controller.getCompteById);
-router.put('/:idCompte', controller.updateCompte);
-router.delete('/:idCompte', controller.deleteCompte);
+comptesRouter.get('/:idCompte', controller.getCompteById);
+comptesRouter.put('/:idCompte', controller.updateCompte);
+comptesRouter.delete('/:idCompte', controller.deleteCompte);
+comptesRouter.get('/:idCompte/solde', controller.getSolde);
 
-router.get('/:idCompte/solde', controller.getSolde);
-
-export default router;
+export default comptesRouter;
